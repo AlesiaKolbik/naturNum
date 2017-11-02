7ac67b86bea54fa0e31388b214e70d171460fe64
alesya.kolbik <moyaalesia@mail.ru>
2017-11-02 18:02:56 +0300

    ++

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,15 +11,20 @@ public class Main {
             System.out.print("Это число не является натуральным.Введите натуральное число: ");
             N = in.nextInt();
         }
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 10; j++) {
-                for (int k = 0; k < 10; k++) {
-                    int sum = i * 100 + j * 10 + k;
-                    int sumCubs = i * i * i + j * j * j + k * k * k;
-                    if (sum == sumCubs && sum < N) {
-                        System.out.print(i + j + k+" ");
-                    }
+        for (int i =10; i < N;i++) {
+            int Per=-1;
+            int sumCubs = 0;
+            while (Per != 0) {
+                Per=i;
+                int a = (Per % 10);
+                if(a==0){
+                 i/10;
                 }
+                sumCubs=sumCubs+ (int)Math.pow(Per, 3);
+                Per = (i - a) / 10;
+            }
+            if (sumCubs == i) {
+                System.out.print(i + " ");
             }
         }
     }
